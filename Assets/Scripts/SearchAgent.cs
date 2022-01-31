9674238f8a9c1f8ad00ff9c94d72cab61b699a19
Meliemau <hwhite@student.neumont.edu>
2022-01-30 22:52:48 -0700

    Fixed Errors

--- Assets/Scripts/SearchAgent.cs
+++ Assets/Scripts/SearchAgent.cs
@@ -16,7 +16,7 @@ public class SearchAgent : Agent
     {
         sourceNode = Node.GetRandomNode<GraphNode>();
         destinationNode = Node.GetRandomNode<GraphNode>();
-        targetNode = initialNode;
+        targetNode = sourceNode;
 
         GeneratePath();
     }
@@ -24,47 +24,21 @@ public class SearchAgent : Agent
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawLine(transform.position, sourceNode.transform.position, Color.green);
+/*        Debug.DrawLine(transform.position, sourceNode.transform.position, Color.green);
         Debug.DrawLine(transform.position, destinationNode.transform.position, Color.red);
         Debug.DrawLine(transform.position + Vector3.up, targetNode.transform.position + Vector3.up, Color.yellow);
-
+*/
         if (targetNode != null)
         {
             movement.MoveTowards(targetNode.transform.position);
         }
     }
 
-/*    public GraphNode GetNextNode(GraphNode graphNode)
-    {
-        if (path.Count == 0) return null;
-
-        int index = path.FindIndex(node => node == graphNode);
-
-        if(index == path.Count - 1)
-        {
-            do
-            {
-            sourceNode = destinationNode;
-            destinationNode = Node.GetRandomNode<GraphNode>();
-
-            } while (sourceNode == destinationNode);
-
-            GeneratePath();
-
-            index = 0;
-            
-        }
-
-        GraphNode nextNode = path[index + 1];
-
-        return nextNode;
-    }*/
-
     public GraphNode GetNextNode(GraphNode graphNode)
     {
         if (path.Count == 0) return null;
-
         int index = path.FindIndex(node => node == graphNode);
+
         if (index == path.Count - 1)
         {
             sourceNode = destinationNode;
@@ -72,13 +46,12 @@ public class SearchAgent : Agent
             {
                 destinationNode = Node.GetRandomNode<GraphNode>();
             } while (sourceNode == destinationNode);
-            GeneratePath();
 
+            GeneratePath();
             index = 0;
         }
 
         GraphNode nextNode = path[index + 1];
-
         return nextNode;
     }
 
