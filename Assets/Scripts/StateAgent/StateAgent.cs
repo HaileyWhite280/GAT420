f8d525b19246ca3d8d780d9c64fac0988c8ed930
Meliemau <hwhite@student.neumont.edu>
2022-02-02 12:00:53 -0700

    Did some things in class

--- Assets/Scripts/StateAgent/StateAgent.cs
+++ Assets/Scripts/StateAgent/StateAgent.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class StateAgent : Agent
 {
+    public StateMachine stateMachine = new StateMachine();
+
     // Start is called before the first frame update
     void Start()
     {
+        stateMachine.AddState(new IdleState(this, "idle"));
+        stateMachine.AddState(new IdleState(this, "patrol"));
+        stateMachine.SetState(stateMachine.StateFromName("idle"));
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        stateMachine.Update();
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            stateMachine.SetState(stateMachine.StateFromName("patrol"));
+
+        }
+
+        if(movement.velocity.magnitude > 0.5f)
+        {
+            animator.SetBool("Walk", true);
+        }
+        else
+        {
+            animator.SetBool("Walk", false);
+        }
     }
 }
